41d736cc82f7fdb9d6b462fbefec86fffeebe6cc
unclebay143 <58919619+unclebay143@users.noreply.github.com>
2020-04-12 19:08:48 -0700

    initial commit- for jideafolayanandco

--- jideafolayanandco/settings.py
+++ jideafolayanandco/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gor!-bmsvic8b2nqe4cp!ye^b)kb%s=fihon0^7sm$l8xlf+=#'
+SECRET_KEY = 'JIDE_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
